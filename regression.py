4c67fdbb2acd7ee99429180c16f9a0d8e96498e5
rmqlife <rmqlife@gmail.com>
2017-09-19 13:15:18 -0400

    update

--- regression.py
+++ regression.py
@@ -1,6 +1,8 @@
 import os, cv2
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.cluster import KMeans
+
 
 def same_size(X,y):
     minlength = min(y.shape[0],X.shape[0])
@@ -18,7 +20,7 @@ def load_model_name(data_name):
     
     
 def load_model_data(pos, feat, num_samples, alpha):
-    inds = np.random.choice(a=min(len(feat),len(pos)),size=num_samples*len(feat))
+    inds = np.random.choice(a=min(len(feat),len(pos)),size=int(num_samples*float(len(feat))))
     if len(inds)%2 == 1:
         inds=inds[:-1]
     nd = len(inds)/2 # number of deltas
@@ -35,17 +37,43 @@ def load_model_data(pos, feat, num_samples, alpha):
 
 def combine_feature(data_name1, data_name2):
     data1 = np.load(data_name1)
-    pos = data1['data']
-    feat = data1['feat']
+    pos = data1['pos']
+    feat1 = data1['feat']
     
     data2 = np.load(data_name2)
     feat2 = data2['feat']
-    print feat1.shape,feat2.shape
-    print feat1[0]
-    
-    print feat2[0]
+
     feat = np.hstack((feat1,feat2))
     return feat, pos
 
+def dist2(p1):
+    return np.sum(p1**2)**0.5
 
+def find_goals(pos,target,n_goals):
+    kmeans = KMeans(n_clusters=n_goals, random_state=0).fit(pos)
+    centers = kmeans.cluster_centers_
+    centers_dist = np.float16(np.ones(centers.shape[0]))
+    centers_id = np.int32(np.zeros(centers.shape[0]))
+
+    for j in range(len(centers)):
+        for i in range(len(pos)):
+            d = dist2(pos[i,:]-centers[j,:])
+            if d<centers_dist[j]:
+                centers_dist[j] = d
+                centers_id[j] = i
+    # sort the distance to final target
+    for i in range(len(centers_id)):
+        centers_dist[i] = dist2(pos[centers_id[i],:]-pos[target,:])
+
+    # sort the centers
+    centers = np.vstack((centers_dist,centers_id)).T
+    centers = centers[centers[:,0].argsort()]
+    centers_dist = centers[:,0]
+    centers_id = np.int32(centers[:,1])
+#     rgblist = get_filelist('/home/rmqlife/work/data/2017-09-09-11-06-56/rgb')
+#     for i in centers_id:
+#         plt.imshow(cv2.imread(rgblist[i],0),cmap='gray')
+#         plt.show()
+    
+    return centers_id.tolist()
 
